93037284dd14ac4f9db184b59778eabeffa4c325
habeeb-an <hbareacode@gmail.com>
2023-04-15 15:57:44 +0530

    destination page image showing error solved

--- client/src/PlaceGallery.jsx
+++ client/src/PlaceGallery.jsx
@@ -8,7 +8,7 @@ export default function PlaceGallery({place}) {
         return (
         
         <div className="absolute  inset-0 bg-black text-white  min-h-screen">
-            <div className="p-8 bg-black grid gap-4 my-4">
+            <div className="p-8 bg-black grid gap-4 my-2">
             <div >
                 <button onClick={()=>setShowAllPhotos(false)} className="top-2 fixed flex bg-white text-black p-2 rounded-2xl  cursor-pointer hover:bg-gray-100">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
@@ -28,41 +28,41 @@ export default function PlaceGallery({place}) {
 
         )
     }
+//<img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} alt=""/>
 
-
-    return (
-        <div>
-        <div className="relative">
+return (
+    <div className="relative">
       <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden">
-        <div >
+        <div className="h-full">
           {place.photos?.[0] && (
-            <div>
-              <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[0]} alt=""/>
+            <div className="h-full object-cover">
+            <img onClick={() => setShowAllPhotos(true)} 
+            
+            className="aspect-square cursor-pointer object-cover h-full" 
+            src={'http://localhost:4000/uploads/' + place.photos[0]} alt=""/>
+
             </div>
           )}
         </div>
         <div className="grid">
           {place.photos?.[1] && (
             <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[1]} alt=""/>
+
           )}
           <div className="overflow-hidden">
             {place.photos?.[2] && (
-                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover relative top-2" src={'http://localhost:4000/uploads/' + place.photos[2]} alt=""/>
+                <img onClick={() => setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/' + place.photos[2]} alt=""/>
+
             )}
           </div>
         </div>
       </div>
-
-                    <button onClick={()=>setShowAllPhotos(true)} className="flex gap-1 bg-white absolute bottom-5 right-5 px-4 py-2 rounded-2xl shadow shadow-md shadow-gray-500 hover:bg-gray-200">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9 4.5v15m6-15v15m-10.875 0h15.75c.621 0 1.125-.504 1.125-1.125V5.625c0-.621-.504-1.125-1.125-1.125H4.125C3.504 4.5 3 5.004 3 5.625v12.75c0 .621.504 1.125 1.125 1.125z" />
-</svg>
-
-                    Show more</button>
-
-
-                </div>
-            
-        </div>
-    )
+      <button onClick={() => setShowAllPhotos(true)} className="flex gap-1 absolute bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-md shadow-gray-500">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+          <path fillRule="evenodd" d="M1.5 6a2.25 2.25 0 012.25-2.25h16.5A2.25 2.25 0 0122.5 6v12a2.25 2.25 0 01-2.25 2.25H3.75A2.25 2.25 0 011.5 18V6zM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0021 18v-1.94l-2.69-2.689a1.5 1.5 0 00-2.12 0l-.88.879.97.97a.75.75 0 11-1.06 1.06l-5.16-5.159a1.5 1.5 0 00-2.12 0L3 16.061zm10.125-7.81a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0z" clipRule="evenodd" />
+        </svg>
+        Show more photos
+      </button>
+    </div>
+  );
 }
