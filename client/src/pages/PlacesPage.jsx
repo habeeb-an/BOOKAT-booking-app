ba46995c1e6a6e9f9dcc046b776a89beb4617bd1
habeeb-an <hbareacode@gmail.com>
2023-04-27 00:48:24 +0530

    places added now can be deleted

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -11,6 +11,11 @@ export default function PlacesPage() {
         setPlaces(data);
     });
 },[]);
+
+function removePhoto(ev,place){
+    ev.preventDefault();
+    setPlaces([...places.filter(name=>name._id!==place._id)])
+}
     return (
         <div>
             <AccountNav/>
@@ -20,12 +25,14 @@ export default function PlacesPage() {
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>Add new place</Link>
             </div>
-            <div className="mt-4">
+            <div className="mt-4 space-y-4">
                 {places.length>0 && places.map((place)=>(
-                    <Link to={'/account/places/'+place._id} className="flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
+                    <Link to={'/account/places/'+place._id} key={place._id} className="relative flex cursor-pointer gap-4 bg-gray-100 p-4 rounded-2xl">
                         <div className="flex w-32 h-32 bg-gray-300 grow shrink-0">
                            <PlaceImg place={place} />
+                           
                         </div>
+                      
                         <div className="grow-0 shrink">
                         <h2 className="text-xl">
                             {place.title}
@@ -35,11 +42,24 @@ export default function PlacesPage() {
                         </p>
                             
                         </div>
+                        <button onClick={ev => removePhoto(ev,place)} className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+            </button>
                         
                     </Link>
                 ))}
+                
             </div>
         </div>
            
     );
 };
+/*
+      <button onClick={ev => removePhoto(ev,link)} className="cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-50 rounded-2xl py-2 px-3">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+              </svg>
+            </button>
+*/ 
